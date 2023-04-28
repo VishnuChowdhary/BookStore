ea793b43ed78ca1df5b8fa0e63330f99ec315378
vishnu26-design <vishnuiron26@gmail.com>
2023-04-28 22:18:49 +0530

    User Registration Done

--- accounts/urls.py
+++ accounts/urls.py
@@ -1,4 +1,6 @@
 from django.urls import path
+from .views import SignupPageView
 
 urlpatterns = [
+    path("singup/", SignupPageView.as_view(), name='signup'),
 ]
--- bookstoreAPP/urls.py
+++ bookstoreAPP/urls.py
@@ -4,6 +4,7 @@ from django.urls import path, include
 urlpatterns = [
     path('admin/', admin.site.urls),
     # Local
-    path('user', include('accounts.urls')),
+    path('accounts/', include('django.contrib.auth.urls')),
+    path('accounts/', include('accounts.urls')),
     path('', include('pages.urls')),
 ]
