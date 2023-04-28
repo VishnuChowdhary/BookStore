252b8bfde576856c5ec99fc8a56bc69dedd1484a
vishnu26-design <vishnuiron26@gmail.com>
2023-04-28 21:39:23 +0530

    Pages App Created

--- bookstoreAPP/urls.py
+++ bookstoreAPP/urls.py
@@ -5,6 +5,5 @@ urlpatterns = [
     path('admin/', admin.site.urls),
     # Local
     path('user', include('accounts.urls')),
-    # APIs
-    path('api/user/', include('accountAPI.urls'))
+    path('', include('pages.urls')),
 ]
